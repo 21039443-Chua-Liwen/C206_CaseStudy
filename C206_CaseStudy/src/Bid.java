3200dac729006aeb81ae70f2bd365a02a06dfe17
phoebe <phoebe@10.174.123.63>
2022-08-01 17:38:50 +0800

    inital src

---
+++ C206_CaseStudy/src/Bid.java
@@ -0,0 +1,37 @@
+public class Bid extends Item {
+	private int bidId; 
+	private String sellerEmail; 
+	private String buyerEmail; 
+	private double bidPrice; 
+	
+	public Bid(String name, String description, int bidId, String sellerEmail, String buyerEmail, double bidPrice) {
+		super(name, description);
+		this.bidId = bidId;
+		this.sellerEmail = sellerEmail;
+		this.buyerEmail = buyerEmail;
+		this.bidPrice = bidPrice;
+	}
+
+	public int getBidId() {
+		return bidId;
+	}
+
+	public String getSellerEmail() {
+		return sellerEmail;
+	}
+
+	public String getBuyerEmail() {
+		return buyerEmail;
+	}
+
+	public double getBidPrice() {
+		return bidPrice;
+	}
+}
+	
+	
+	
+	
+	
+	
+
---
+++ C206_CaseStudy/src/Item.java
@@ -0,0 +1,41 @@
+public class Item {
+	private String name; 
+	private String description; 
+	private double minbidprice; 
+	private String startdate; 
+	private String enddate;
+	private double bidincrement; 
+	
+	public Item (String name, String description) {
+		this.name = name;
+		this.description = description;
+		this.minbidprice = 0;
+		this.startdate = "";
+		this.enddate = "";
+		this.bidincrement = 0;
+	}
+	
+	public String getName() { 
+		return name; 
+	}
+	
+	public String getDescription() { 
+		return description; 
+	}
+
+	public double getMinbidprice() {
+		return minbidprice;
+	}
+
+	public String getStartdate() {
+		return startdate;
+	}
+
+	public String getEnddate() {
+		return enddate;
+	}
+
+	public double getBidincrement() {
+		return bidincrement;
+	}
+}
